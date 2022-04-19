90ae3e2e514622edeb47d9254bfdb3f233a79fae
Eric Levin <eric@Erics-MacBook-Pro.local>
2022-04-18 23:24:39 -0500

    artifact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, Suspense } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import Artifact from './Artifact'
 
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
@@ -27,11 +29,15 @@ function Box(props) {
 export default function App() {
   return (
     <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+        <OrbitControls/>
+         <Suspense fallback={null}>
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+            <Artifact/>
+        </Suspense>
     </Canvas>
   )
 }
---
+++ src/Artifact.js
@@ -0,0 +1,98 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import artifactUrl from './assets/artifact.glb'
+
+export default function Model(props) {
+  const group = useRef();
+  const { nodes, materials } = useGLTF(artifactUrl);
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Base.geometry}
+        material={nodes.Base.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.inner_ring.geometry}
+        material={nodes.inner_ring.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.base_ring.geometry}
+        material={nodes.base_ring.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.endcap.geometry}
+        material={nodes.endcap.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.endcap001.geometry}
+        material={nodes.endcap001.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.connectors.geometry}
+        material={nodes.connectors.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Inner_Sphere.geometry}
+        material={nodes.Inner_Sphere.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.inner_ring001.geometry}
+        material={nodes.inner_ring001.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.inner_ring002.geometry}
+        material={nodes.inner_ring002.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.inner_ring003.geometry}
+        material={nodes.inner_ring003.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.inner_ring004.geometry}
+        material={nodes.inner_ring004.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.spin_ring.geometry}
+        material={nodes.spin_ring.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.inner_ring007.geometry}
+        material={nodes.inner_ring007.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.pyramid.geometry}
+        material={nodes.pyramid.material}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload(artifactUrl);
